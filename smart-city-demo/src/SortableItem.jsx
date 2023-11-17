005e749b4f5c37717aff2d8056af95a5341862e5
Sasha Weir <sashasalzweir@gmail.com>
2023-11-17 21:22:15 +0100

    Pulled the ordering game out into its own componant and created a router so we can have multiple pages as well as a home page

--- smart-city-demo/src/SortableItem.jsx
+++ smart-city-demo/src/SortableItem.jsx
@@ -4,31 +4,29 @@ import React from "react";
 import { Card } from "react-bootstrap";
 
 export default function SortableItem(props) {
-    
-    const {
-        attributes,
-        listeners,
-        setNodeRef,
-        transform,
-        transition
-    } = useSortable({ id: props.id });
+  const { attributes, listeners, setNodeRef, transform, transition } =
+    useSortable({ id: props.id });
 
-    const style = {
-        transform: CSS.Transform.toString(transform),
-        transition,
-    }
-    const className = props.className;
-    if (props.className === "correct"){
-        
-        return (<div ref={setNodeRef} style={style} {...attributes} {...listeners}>
-            <Card body className={className} >{props.id} {props.percentage}</Card>
-        </div>)
-    }
-
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+  };
+  const className = props.className;
+  if (props.className === "correct") {
     return (
-        <div ref={setNodeRef} style={style} {...attributes} {...listeners}>
-            <Card body className={className}>{props.id}</Card>
+      <div ref={setNodeRef} style={style} {...attributes} {...listeners}>
+        <Card body className={className}>
+          {props.id} {props.percentage}
+        </Card>
+      </div>
+    );
+  }
 
-        </div>
-    )
+  return (
+    <div ref={setNodeRef} style={style} {...attributes} {...listeners}>
+      <Card body className={className}>
+        {props.id}
+      </Card>
+    </div>
+  );
 }
